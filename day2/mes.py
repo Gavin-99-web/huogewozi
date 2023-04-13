82555c7eb9a0bdd74b5e5baf7f761807f0cccc20
Gavin-99-web <81733672+Gavin-99-web@users.noreply.github.com>
2023-04-13 12:57:56 +0800

    Add files via upload

---
+++ day2/mes.py
@@ -0,0 +1,6 @@
+class MesID:
+    url = "https://ceshiren.com/"
+    search_input = """//div[@class="search-bar"]/input"""
+    search_button = "search-button"
+    advanced_search_class = "searching"
+    button = """//div[@class="search-bar"]/button"""
