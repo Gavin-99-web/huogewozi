ce9e81bda462aa288b0fb9b62775f3d551f1b1ab
Gavin-99-web <81733672+Gavin-99-web@users.noreply.github.com>
2023-04-25 20:25:58 +0800

    Delete mes.py

--- day2/mes.py
+++
@@ -1,6 +0,0 @@
-class MesID:
-    url = "https://ceshiren.com/"
-    search_input = """//div[@class="search-bar"]/input"""
-    search_button = "search-button"
-    advanced_search_class = "searching"
-    button = """//div[@class="search-bar"]/button"""
